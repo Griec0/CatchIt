ec9e21581ca9f2f079f650b06e1c174c4e0e903a
Grieco <94195660+Griec0@users.noreply.github.com>
2024-12-10 16:50:43 +0100

    UI Update

--- CatchIt/CatchIt/ContentView.swift
+++ CatchIt/CatchIt/ContentView.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        HStack{
+            Text("Back")
+                .foregroundStyle(.blue)
+            Spacer()
+            Text("Settings")
+            Spacer()
+            Image(systemName: "gear")
+                .foregroundStyle(.blue)
+        }.padding()
+        
+        List {
+            Text("Setting 1")
+            Text("Setting 2")
+            Text("Setting 3")
+            Text("Setting 4")
         }
-        .padding()
     }
 }
 
