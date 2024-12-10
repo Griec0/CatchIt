4cf633b9ad8d3130cfa8e390dbec73bc4f116d12
Grieco <94195660+Griec0@users.noreply.github.com>
2024-12-10 12:26:00 +0100

    Xcode folder

---
+++ CatchIt/CatchItTests/CatchItTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatchItTests.swift
+//  CatchItTests
+//
+//  Created by Giano on 10/12/24.
+//
+
+import Testing
+@testable import CatchIt
+
+struct CatchItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
